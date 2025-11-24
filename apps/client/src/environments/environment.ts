6e9d720480d540318a94b0d010b191674e7e4560
Giovani <giovanikloussey@gmail.com>
2025-11-24 14:13:12 +0100

    inté supabase

---
+++ apps/client/src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://pydevazrwdzouhiakidn.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZGV2YXpyd2R6b3VoaWFraWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5MjQxNDAsImV4cCI6MjA3OTUwMDE0MH0.LHDIgtx0000uG2YcJuoeU0uI8dhjwr4FTxLr-HGWKWk'
+};
